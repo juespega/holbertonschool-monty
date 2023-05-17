f1e6f2ced72b50c4444416b97fa2f4d432ba6808
juespega <juespega666@gmail.com>
2023-05-17 13:44:13 -0700

    Implement the push and pall opcodes.

---
+++ monty.c
@@ -0,0 +1,55 @@
+#include "monty.h"
+
+/**
+ * main - Executes de commands of a monty file
+ * @argc: Size of argv
+ * @argv: Array of arguments
+ *
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char **argv)
+{
+
+    unsigned int line_number = 1;
+    char *command = NULL;
+    char *line = NULL;
+
+    stack_t *stack = NULL;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for(; (line = lines_reader(argv[1], line_number)) != NULL; line_number++)
+    {
+        // printf("%s\n", line);
+        
+        command = strtok(line, " \n");
+        /*
+        
+        if (command == NULL)
+
+        */
+        if (strcmp(command, "push") == 0)
+        {
+            push(&stack, line_number);
+        }
+
+        if (strcmp(command, "pall") == 0)
+        {
+            pall(&stack, line_number);
+        }
+
+        if (strcmp(command, "nop") == 0)
+        {
+            nop(&stack, line_number);
+        }
+
+        
+        free(line);
+    }
+
+    return (EXIT_SUCCESS);
+}
