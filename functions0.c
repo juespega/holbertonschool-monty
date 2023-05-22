3577804091909a28e15257bfc3bb174739960b05
Julián Esteban Pérez Gallego <54298047+juespega@users.noreply.github.com>
2023-05-21 21:21:37 -0500

    Update functions0.c

--- functions0.c
+++ functions0.c
@@ -137,36 +137,3 @@ void swap(stack_t **stack, unsigned int line_number)
 	temp_node->prev = NULL;
 	*stack = temp_node;
 }
-/**
- * add - Adds a new node at the beginning of a stack_t list.
- * @stack: Double pointer to the first node of the list.
- * @line_number: Data value for the new node.
- *
- * Return: NULL on failure or address of the new element on success.
- */
-void add(stack_t **stack, unsigned int line_number)
-{
-int ans;
-
-if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-{
-	dprintf(STDERR_FILENO, "L%u: can't add, stack too short\n", line_number);
-	free(line);
-	stack_freer(*stack);
-	exit(EXIT_FAILURE);
-}
-ans = (*stack)->next->n + (*stack)->n;
-(*stack)->next->n = ans;
-pop(stack, line_number);
-}
-/**
- * nop - Adds a new node at the beginning of a stack_t list.
- * @stack: Double pointer to the first node of the list.
- * @line_number: Data value for the new node.
- *
- * Return: NULL on failure or address of the new element on success.
- */
-void nop(stack_t **stack, unsigned int line_number)
-{
-	(void)stack, (void)line_number;
-}
