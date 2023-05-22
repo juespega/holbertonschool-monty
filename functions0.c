24b8958b5e94fe58d0675214ee10636d7b7de864
Julián Esteban Pérez Gallego <54298047+juespega@users.noreply.github.com>
2023-05-21 21:20:14 -0500

    Update functions0.c

--- functions0.c
+++ functions0.c
@@ -146,18 +146,18 @@ void swap(stack_t **stack, unsigned int line_number)
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	int ans;
-	
-	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-    	{
-		dprintf(STDERR_FILENO, "L%u: can't add, stack too short\n", line_number);
-		free(line);
-		stack_freer(*stack);
-		exit(EXIT_FAILURE);	
-	}
-	ans = (*stack)->next->n + (*stack)->n;
-	(*stack)->next->n = ans;
-	pop(stack, line_number);
+int ans;
+
+if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+{
+	dprintf(STDERR_FILENO, "L%u: can't add, stack too short\n", line_number);
+	free(line);
+	stack_freer(*stack);
+	exit(EXIT_FAILURE);
+}
+ans = (*stack)->next->n + (*stack)->n;
+(*stack)->next->n = ans;
+pop(stack, line_number);
 }
 /**
  * nop - Adds a new node at the beginning of a stack_t list.
