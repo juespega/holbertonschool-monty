9ef7bf1f6140560ca551bcf98f7432a4cb35b3d7
Julián Esteban Pérez Gallego <54298047+juespega@users.noreply.github.com>
2023-05-21 21:26:32 -0500

    Update lines_reader.c

--- lines_reader.c
+++ lines_reader.c
@@ -14,13 +14,13 @@ char *lines_reader(char *file, unsigned int line_number)
 	ssize_t number_of_characters_read = 0;
 	unsigned int i;
 
-	file_monty = fopen(file,"r");
+	file_monty = fopen(file, "r");
 	if (file_monty == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", file); 
-		exit(EXIT_FAILURE); 
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", file);
+		exit(EXIT_FAILURE);
 	}
-	for(i = 0; i < line_number; i++)
+	for (i = 0; i < line_number; i++)
 	{
 		number_of_characters_read = getline(&line, &lenght_of_line, file_monty);
 		if (number_of_characters_read == -1)
