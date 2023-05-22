6938a48e06fe57a021010926aa8335c88a56a16f
Julián Esteban Pérez Gallego <54298047+juespega@users.noreply.github.com>
2023-05-21 20:35:15 -0500

    Update get_opcode.c

--- get_opcode.c
+++ get_opcode.c
@@ -1,25 +1,25 @@
 #include "monty.h"
 /**
- * pint - Adds a new node at the beginning of a stack_t list.
+ * void - Adds a new node at the beginning of a stack_t list.
  * @instruction: Data value for the new node.
  * @stack: Double pointer to the first node of the list.
  * @line_number: Data value for the new node.
  *
- * Return: NULL on failure or address of the new element on success
+ * Return: NULL on failure or address of the new element on success.
  */
-void(*get_opcode(char *instruction))(stack_t **stack, unsigned int line_number)
+void (*get_opcode(char *instruction))(stack_t **stack, unsigned int line_number)
 {
-unsigned int i;
-instruction_t instructions[] = {
-{"push", push},
-{"pall", pall},
-{"nop", nop},
-{NULL, NULL}
-};
-for (i = 0; instructions[i].opcode != NULL; i++)
-{
-if (strcmp(instructions[i].opcode, instruction) == 0)
-return (instructions[i].f);
-}
-return (NULL);
+	unsigned int i;
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{"nop", nop},
+		{NULL, NULL}
+	};
+	for (i = 0; instructions[i].opcode != NULL; i++)
+	{
+		if (strcmp(instructions[i].opcode, instruction) == 0)
+			return (instructions[i].f);
+	}
+	return (NULL);
 }
