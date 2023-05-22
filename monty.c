074904c41a614bebe10b542cdab566f8de028c95
Julián Esteban Pérez Gallego <54298047+juespega@users.noreply.github.com>
2023-05-21 20:40:04 -0500

    Update monty.c

--- monty.c
+++ monty.c
@@ -21,6 +21,7 @@ int main(int argc, char **argv)
 	}
 	while ((line = lines_reader(argv[1], line_number)) != NULL)
 	{
+		
 		command = strtok(line, " \r\t\n");
 		if (command == NULL)
 		{
@@ -32,18 +33,43 @@ int main(int argc, char **argv)
 			var = 1;
 		else if (strcmp(command, "pall") == 0)
 			var = 2;
+		else if (strcmp(command, "pint") == 0)
+			var = 3;
+		else if (strcmp(command, "pop") == 0)
+			var = 4;
+		else if (strcmp(command, "swap") == 0)
+			var = 5;
+		else if (strcmp(command, "add") == 0)
+			var = 6;
+		else if (strcmp(command, "nop") == 0)
+			var = 7;
 		else
 			var = 0;
 		switch (var)
 		{
-			case 1:
-				push(&stack, line_number);
-				break;
-			case 2:
-				pall(&stack, line_number);
-				break;
-			default:
-	dprintf(STDERR_FILENO, "L%u: unknown instruction %s\n", line_number, command);
+		case 1:
+			push(&stack, line_number);
+			break;
+		case 2:
+			pall(&stack, line_number);
+			break;
+		case 3:
+			pint(&stack, line_number);
+			break;
+		case 4:
+			pop(&stack, line_number);
+			break;
+		case 5:
+			swap(&stack, line_number);
+			break;
+		case 6:
+			add(&stack, line_number);
+			break;
+        	case 7:
+			nop(&stack, line_number);
+			break;
+		default:
+			dprintf(STDERR_FILENO, "L%u: unknown instruction %s\n", line_number, command);
 			exit(EXIT_FAILURE);
 		}
 		free(line);
